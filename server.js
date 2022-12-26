6c004626249abfef64f36439607e650b838dcf11
Chloe Kurdziel <chloekurdziel@gmail.com>
2022-12-26 00:22:03 -0500

    added new controllers

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 
 // ROOT
 app.get('/', (req, res) => {
@@ -13,6 +19,8 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+
 
 // LISTEN
 app.listen(process.env.PORT, () => {
